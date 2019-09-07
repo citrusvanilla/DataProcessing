36e7d6d519ee713717284321917c93694e086bf1
justin fung <justinfung@justins-MacBook-Pro.local>
2019-09-07 16:21:49 -0700

    formatting

--- data_processing.py
+++ data_processing.py
@@ -5,42 +5,42 @@ from modes import resolvers
 
 
 def lambda_handler(event: dict, context: dict) -> dict:
-    """
-    Main handler for the DataProcessing lambda.
+  """
+  Main handler for the DataProcessing lambda.
 
-      Makes connection to dynamoDB.
-      Passes API key as event to dynamo and returns raw data.
-      Processes raw data and returns to calling context.
+    Makes connection to dynamoDB.
+    Passes API key as event to dynamo and returns raw data.
+    Processes raw data and returns to calling context.
 
-    Args:
-      event:         dict containing API key.
-      context:       dict containing calling context info.
-    
-    Returns:
-      processedData: dict containing processed data
-    """
-    # Make a connection to dynamodb service with boto3.
-    client = boto3.client('dynamodb')
+  Args:
+    event:         dict containing API key.
+    context:       dict containing calling context info.
+  
+  Returns:
+    processedData: dict containing processed data
+  """
+  # Make a connection to dynamodb service with boto3.
+  client = boto3.client('dynamodb')
 
-    # getItem api call, pass in params
-    response = client.get_item(
-        TableName = "UserData",
-        Key = {
-            'UserID': {
-                'S': event['UserID']
-            }
-        }
-    )
-
-    # Extract data from the response.
-    data = response
-
-    # Process data.
-    processedData: dict = {
-      'overview': overview.get_overview_data(data),
-      'resolvers': resolvers.get_resolvers_data(data)
+  # getItem api call, pass in params
+  response = client.get_item(
+    TableName = "UserData",
+    Key = {
+      'UserID': {
+          'S': event['UserID']
+      }
     }
-    
-    # Return
-    return processedData
+  )
+
+  # Extract data from the response.
+  data = response
+
+  # Process data.
+  processedData: dict = {
+    'overview': overview.get_overview_data(data),
+    'resolvers': resolvers.get_resolvers_data(data)
+  }
+  
+  # Return
+  return processedData
 
